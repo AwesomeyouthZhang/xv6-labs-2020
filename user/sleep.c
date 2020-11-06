e7ddf2f6818d346406775f768f7d1c319a90344b
AwesomeyouthZhang <zhang.zx.1073538693@gmail.com>
2020-11-06 21:58:44 +0800

    sleep

---
+++ user/sleep.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char* argv[]) {
+	if(argc != 2) 
+	{
+		fprintf(2, "sleep <number>\n");
+		exit(1);
+
+	}
+	int number = atoi(argv[1]);
+	sleep(number);
+	exit(0);
+
+
+}
+
